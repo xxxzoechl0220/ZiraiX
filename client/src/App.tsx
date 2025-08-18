a6ae9cba307314d4e391dc5298350960707f07bf
xxxzoechl0220 <xxxzoechl0220@gmail.com>
2025-08-18 16:35:20 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
 import Home from "@/pages/Home";
 import AboutPage from "@/pages/About";
 import TeamPage from "@/pages/Team";
@@ -22,7 +21,12 @@ function Router() {
       <Route path="/privacy" component={PrivacyPage} />
       <Route path="/product" component={ProductPage} />
       <Route path="/contact" component={ContactPage} />
-      <Route component={NotFound} />
+      <Route>
+        {() => {
+          window.location.href = "/";
+          return null;
+        }}
+      </Route>
     </Switch>
   );
 }
