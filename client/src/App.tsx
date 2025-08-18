f9588a07ba73ccf50fe316f6995d6c4eac148320
xxxzoechl0220 <xxxzoechl0220@gmail.com>
2025-08-18 16:30:14 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
+import NotFound from "@/pages/NotFound";
 import Home from "@/pages/Home";
 import AboutPage from "@/pages/About";
 import TeamPage from "@/pages/Team";
